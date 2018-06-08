93b0abd26be411c2af679b2ea6804c132ed1691f
Ubuntu <paul@newknowledge.io>
2018-06-08 16:58:38 +0000

    passing volumes to super as suggested, 02

--- DukeD3MWrapper/wrapper.py
+++ DukeD3MWrapper/wrapper.py
@@ -84,7 +84,7 @@ class duke(PrimitiveBase[Inputs, Outputs, Params, Hyperparams]):
         'primitive_family': metadata_base.PrimitiveFamily.DATA_CLEANING,
     })
     
-    def __init__(self, *, hyperparams: Hyperparams, random_seed: int = 0, volumes: typing.Dict=None)-> None:
+    def __init__(self, *, hyperparams: Hyperparams, random_seed: int = 0, volumes: typing.Dict[str,str]=None)-> None:
         super().__init__(hyperparams=hyperparams, random_seed=random_seed, volumes=volumes)
                 
         self._decoder = JSONDecoder()
