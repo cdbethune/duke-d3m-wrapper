07fcff4700e65f75f1535093463c6198ef9a5eef
azunre <azunre@PaulLinuxGPUDev.jaavmej2qrwupdw4djg0r2u5ud.bx.internal.cloudapp.net>
2018-11-29 23:08:24 +0000

    added preprocessing to solve accuracy issues due to d3m preprocessing, 05

--- DukeD3MWrapper/wrapper.py
+++ DukeD3MWrapper/wrapper.py
@@ -115,15 +115,18 @@ class duke(PrimitiveBase[Inputs, Outputs, Params, Hyperparams]):
         # cast frame data type back to original, if numeric, to ensure
         # that duke can drop them, and not skew results (since d3m
         #  preprocessing prims turn everything into str/object)
-        # for i in range(frame.value.shape[1]):
-        #     print("DEBUG::types:")
-        #     print(frame.value.metadata.query_column(i)['semantic_types'][0])
-        #     if (frame.value.metadata.query_column(i)['semantic_types'][0]=='http://schema.org/Integer'):
-        #         frame.value = frame.value.astype({frame.value.columns[i]:int})
-        #     elif (frame.value.metadata.query_column(i)['semantic_types'][0]=='http://schema.org/Float'):
-        #         frame.value = frame.value.astype({frame.value.columns[i]:float})
-        #     elif (frame.value.metadata.query_column(i)['semantic_types'][0]=='https://metadata.datadrivendiscovery.org/types/CategoricalData'):
-        #         frame.value = frame.value.drop(columns=[frame.value.columns[i]])
+        tmp = frame.value
+        for i in range(frame.value.shape[1]):
+            print("DEBUG::types:")
+            print(frame.value.metadata.query_column(i)['semantic_types'][0])
+            print(frame.value.columns[i])
+            if (frame.value.metadata.query_column(i)['semantic_types'][0]=='http://schema.org/Integer'):
+                tmp = tmp.astype({frame.value.columns[i]:int})
+            elif (frame.value.metadata.query_column(i)['semantic_types'][0]=='http://schema.org/Float'):
+                tmp.ix[:,frame.value.columns[i]].replace('',0,inplace=True)
+                tmp = tmp.astype({frame.value.columns[i]:float})
+            elif (frame.value.metadata.query_column(i)['semantic_types'][0]=='https://metadata.datadrivendiscovery.org/types/CategoricalData'):
+                tmp = tmp.drop(columns=[frame.value.columns[i]])
 
         #print('beginning summarization... \n')
 
@@ -140,7 +143,7 @@ class duke(PrimitiveBase[Inputs, Outputs, Params, Hyperparams]):
         verbose=True
 
         duke = DatasetDescriptor(
-            dataset=frame,
+            dataset=tmp,
             tree=tree_path,
             embedding=embedding_path,
             row_agg_func=row_agg_func,
